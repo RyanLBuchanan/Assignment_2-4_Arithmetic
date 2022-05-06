624284ad397d6798a261ea29cc143ad6b60a53af
Ryan Buchanan <buchanan.ryan22@gmail.com>
2022-05-06 11:42:17 -0600

    feat: Completed A2-4 Arithmetic

---
+++ Assignment_2-4_Arithmetic/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Assignment_2_4_Arithmetic
+{
+    class Addition
+    {
+        // Main method begins execution of C# app
+        static void Main()
+        {
+            // Declare variables
+            int number1;
+            int number2;
+            int sum;
+
+            // Prompt user
+            Console.WriteLine("Enter first integer: ");
+            // Read first integer from user
+            number1 = int.Parse(Console.ReadLine());
+
+            // Prompt user
+            Console.WriteLine("Enter second integer: ");
+            // Read second integer from user
+            number2 = int.Parse(Console.ReadLine());
+
+            // Sum integers
+            sum = number1 + number2;
+
+            // Display sum
+            Console.WriteLine($"Sum is {sum}");
+        } // End Main
+    } // End Class Addition
+}
